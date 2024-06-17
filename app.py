4b1ce6c4168673ca9f26f4008c3585bad742ec57
Hiro Funatsuka <hiro.funatsuka@gmail.com>
2024-06-17 11:16:04 +0100

    Add a functionality of adding a todo

--- app.py
+++ app.py
@@ -1,12 +1,38 @@
-from flask import Flask, jsonify
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+import os
 
+load_dotenv(override=True)
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = os.getenv(
+    'SQLALCHEMY_TRACK_MODIFICATIONS')
+
+db = SQLAlchemy(app)
 
 
-@app.route('/', methods=["GET"])
-def hello_world():
-    return jsonify({'message': 'Hello, world'})
+class ToDo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    todo = db.Column(db.String(128), nullable=False)
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+@app.route('/')
+def index():
+    data = ToDo.query.all()
+    return render_template('todo.html', data=data)
+
+
+@app.route('/add', methods=['POST'])
+def add():
+    todo = request.form['todo']
+    new_todo = ToDo(todo=todo)
+    db.session.add(new_todo)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+
+if __name__ == '__main__':
+    # with app.app_context():
+    #     db.create_all()
+    app.run()
